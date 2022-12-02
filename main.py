b5411f7e7238a46ffd5203c6f892c6d708d0014c
Leonid777999 <lskrypnikov@n-ix.com>
2022-12-02 17:15:50 +0200

    Was done in 02.12

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get(url="https://www.jetbrains.com/help/pycharm/manage-branches.html")
-print("test commit")
+
+# 1.variables
+a = "12"
+b = 13
+c = 14.0
+print(a)
+print(b)
+print(c)
+
+
+# 2.casting
+d = int(5)
+e = float(5.0)
+f = str("5")
+print(d/2)
+print(e/2)
+print(int(f)/2)    #casting string to int to be divided
+
+
+# 3.
+g =int(5)
+h =int(8.0)
+i =int("7")
+print(g)
+print(h)  #without 0 because int
+print(i)  #without brackets because int
+
+
+# 4.
+hello = "Hello, world!"
+
+
+
+# 5.
+name = "John"
+age = "30"
+text = "Hello, my name is %s, I'm %s"
+print(text %(name,age))
