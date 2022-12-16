7b79975a3810fadf3f05d1e6d6aeda2f9ca16ed2
Leonid777999 <lskrypnikov@n-ix.com>
2022-12-16 10:36:32 +0200

    Add exercises from 1 to 3 in COMPREHENSION

--- main.py
+++ main.py
@@ -277,3 +277,35 @@ def factorial(n):
 factorial(6)
 
 
+#LambdaLambdaLambdaLambdaLambdaLambdaLambdaLambdaLambdaLambdaLambda
+
+l_list = [1,2,3,4,5]
+
+
+
+
+
+#COMPREHENSIONCOMPREHENSIONCOMPREHENSIONCOMPREHENSIONCOMPREHENSIONCOMPREHENSIONCOMPREHENSIONCOMPREHENSION
+
+# 1.
+numbers_list = [1, -2.0, 3, -4.34, 5]
+negative_list = [int(x) for x in numbers_list if x<0 ]
+
+print(negative_list)
+
+# 2.
+string_list = ["string", "mediumstring", 'longstring', "hugestring"]
+lenght_list = [x for x in string_list if len(x) == 10]
+
+print(lenght_list)
+
+# 3.
+thirteen_list = [3, 26, 34, 39, 56]
+divisible_list = [x for x in thirteen_list if x%13==0]
+
+print(divisible_list)
+
+# 4.
+name_list = ["Jack", "john", "Paul", "sean"]
+
+
