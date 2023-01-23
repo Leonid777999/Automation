877abe8eead7b27f86b9896d985c41b78814660e
Leonid777999 <lskrypnikov@n-ix.com>
2023-01-23 19:09:06 +0200

    Added examples to the 3,4 tasks in for the class section and 5tH task

--- Class, attributes, constructor and methods.py
+++ Class, attributes, constructor and methods.py
@@ -11,21 +11,35 @@ class Test:
     def __str__(self):
         return f"({self.url, self.login, self.password})"
 
-
-    def choice(self):
+#3.
+    def make_choice(self):
         choice = input("Your choice:")
         ad = 5
-        if choice == Test.type_m:                      #???????????
+        if choice == Test.type_m:
             print("You are welcome")
         else:
             print("Ask another team")
 
+    def new_login(self):
+        new_login = self.login + str(1)
+        return new_login
+
+    def new_password(self,add):
+        new_password = self.password + int(add)
+        return new_password
 
 
 #4.
-aa = Test("www.test.com", "entry", 123)
+test_object = Test("www.test.com", "entry", 123)
+test_object1 = Test("www.test111", "comeon", 234)
 
-print(aa)
+# check for the second task
+print(test_object)
 
-aa.choice()
+# check for the third task
+test_object.make_choice()
+
+test_object.new_login()
+
+test_object1.new_password(456)
 
