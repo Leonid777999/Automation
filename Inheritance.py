69e508af52488561bf02e3ea63abbf8c6c1d0063
Leonid777999 <lskrypnikov@n-ix.com>
2023-01-30 21:01:32 +0200

    Changed a little parent class and added first and second task to the Inheritance page

--- Inheritance.py
+++ Inheritance.py
@@ -1,8 +1,44 @@
 from classes import Test
+from selenium import webdriver
 
-class Mobile(Test):
-    platform = "Android"
+class Web(Test):
+    platform = "Chrome"
+    browser = webdriver.Chrome()
 
+
+    def __init__(self, new_login, url, login, password):
+        super().__init__(url, login, password)
+        self.new_login = new_login
+    def make_choice(self):
+        choice1 = input("Your choice:")
+        if choice1 == super().type:
+            choice2 = input("Your platform")
+            if choice2 == Web.platform:
+                print("You are welcome!")
+            else:
+                print("Ask another team!")
+        else:
+            print("Ask another team!")
+
+    def go_to(self):
+        return Web.browser.get(self.url)
+
+    def extra_login(self):
+        extra_login = super().new_login() + "55+"
+        return extra_login
+
+
+obj = Web("mobile","https://www.github.com","login","password")
+thor1 = Web("mob","https://www.github.com", "login", 56788)
+
+# check for the task 2
+thor1.make_choice()
+
+thor1.extra_login()
+
+thor1.new_password(10000)
+
+thor1.new_login()
 
 
 
--- classes.py
+++ classes.py
@@ -1,32 +1,33 @@
 # 1.
 class Test:
-    type = "mobile application"
+    type = "web application"
 
-    def __init__(self, url, login, password):
+    def __init__(self,url, login, password):
         self.url = url
         self.login = login
         self.password = password
 #2.
-    def __str__(cls):
-        return f"({cls.url, cls.login, cls.password})"
+    def real_print(cls):
+        return str(f"({cls.url, cls.login, cls.password})")
 
 #3.
     def make_choice(self):
         choice = input("Your choice:")
-        ad = 5
-        if choice == Test.type_m:
+        if choice == Test.type:
             print("You are welcome")
         else:
             print("Ask another team")
 
     def new_login(self):
-        new_login = self.login + str(1)
+        new_login = str(self.login) + str(8)
         return new_login
 
     def new_password(self,add):
         new_password = self.password + int(add)
         return new_password
 
+    def print_new(self):
+        return f"({self})"
 
 
 
@@ -59,5 +60,5 @@ print(test_object.__dict__)
 setattr(test_object1,"captcha","true")
 getattr(test_object1,"captcha")
 
-print(test_object1)     #??? how to print current object with the added attribute without key
+test_object.print_new()     #??? how to print current object with the added attribute without key
 
