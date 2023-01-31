ceb8a1e2e7af3519bbd8a8eecb56205d0b883c8c
Leonid777999 <lskrypnikov@n-ix.com>
2023-01-31 19:22:50 +0200

    Add first task to the Encapsulation section

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,2 +1,22 @@
 class First:
-    pass
+
+    def __init__(self, first_protected, first_private):
+        self._first_protected = first_protected
+        self.__first_private = first_private
+
+    def _protected_method(self):
+        return self.__dict__
+
+    def __private_method(self):
+        return self.__dict__
+
+
+
+
+
+first = First("protected", "private")
+
+first._first_protected
+first._First__first_private
+
+first._protected_method()
