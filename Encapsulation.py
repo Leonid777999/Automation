e063efefe334381a50010afff00ccb358b247d0a
Leonid777999 <lskrypnikov@n-ix.com>
2023-02-01 20:10:14 +0200

    Added second task to the Encapsulation section

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,3 +1,4 @@
+#1 111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
 class First:
 
     def __init__(self, first_protected, first_private):
@@ -10,13 +11,28 @@ class First:
     def __private_method(self):
         return self.__dict__
 
+#2 2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+class Second:
+    def __init__(self, first_protected,first_private,second_public):
+        super().__init__(first_protected,first_private,second_public)
+        self.second_public = second_public
 
 
 
+second = Second("protected", "private", "public")
+second.first_protected
+
+#3 33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
 
 first = First("protected", "private")
+
+first
 
 first._first_protected
 first._First__first_private
 
 first._protected_method()
+first._First__private_method()
+
+
+
