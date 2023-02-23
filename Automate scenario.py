cc3ba3d5f5a53eedcde0e2e9cbafbae353617e5a
Leonid777999 <lskrypnikov@n-ix.com>
2023-02-23 19:06:37 +0200

    Continued first task

--- Automate scenario.py
+++ Automate scenario.py
@@ -2,16 +2,23 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
-login = "/html/body/div[5]/div[3]/div[3]/div[2]/div[1]/div[3]/form/ul/li[1]/p[2]/input"
-password = "/html/body/div[5]/div[3]/div[3]/div[2]/div[1]/div[3]/form/ul/li[1]/input"
-enter = "/html/body/div[5]/div[3]/div[3]/div[2]/div[1]/div[3]/form/p/input"
+login = "//input[@name='login']"
+password = "//input[@name='pass']"
+enter = "//input[@tabindex='5']"
+email_last = []
+email_before_last = []
+
 open_site = browser.get("https://www.i.ua")
-browser.find_element("xpath","/html/body/div[3]/div[3]/div[3]/div[2]/div[1]/div[3]/form/ul/li[1]/p[2]/input").send_keys("automate")
-browser.find_element("name","pass").send_keys("automate123")
-browser.find_element("xpath","//p/input").click()
+browser.find_element("xpath",login).send_keys("automate")
+browser.find_element("xpath",password).send_keys("automate123")
+browser.find_element("xpath",enter).click()
+find_email1 = browser.find_element("xpath","//*[@id='mesgList']/form/div[1]/a/span[3]/span")
+find_email2 = browser.find_element("xpath","//*[@id='mesgList']/form/div[2]/a/span[3]/span")
+email_last.append(find_email1.text)
+email_before_last.append(find_email2.text)
 
-
-
+print(email_last)
+print(email_before_last)
 
 
 
