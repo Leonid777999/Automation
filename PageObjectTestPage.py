a44699c4baced77b6c1594ed60c2538920c2e48c
Leonid777999 <lskrypnikov@n-ix.com>
2023-02-28 18:58:25 +0200

    Added methods to the test page

--- PageObjectTestPage.py
+++ PageObjectTestPage.py
@@ -3,5 +3,6 @@ from Pages.PageObjectLoginPage import LoginPage
 def test_excercise(init_browser):
     site_main_page = LoginPage(init_browser)
     site_main_page.enter_user_login("automate")
-
+    site_main_page.enter_user_password("automate123")
+    site_main_page.click_on_enter_button()
 
--- Pages/PageObjectMailBoxPage.py
+++ Pages/PageObjectMailBoxPage.py
@@ -1,7 +1,9 @@
+from PageObjectBaseClass import BaseClass
 
+class MailBox(BaseClass):
 
-class MailBox:
-    pass
+    def __init__(self, browser):
+        super().__init__(browser)
 
 email_last = "//*[@id='mesgList']/form/div[1]/a/span[3]/span"
 email_before_the_last = "//*[@id='mesgList']/form/div[2]/a/span[3]/span"
