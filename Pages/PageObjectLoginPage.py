c2b6dd203dfe66dfc8200638f2209d62e40eaf6a
Leonid777999 <lskrypnikov@n-ix.com>
2023-02-28 18:53:04 +0200

    Added method "click_on_enter_button"

--- Pages/PageObjectLoginPage.py
+++ Pages/PageObjectLoginPage.py
@@ -7,12 +7,14 @@ class LoginPage(BaseClass):
 
     user_login = By.XPATH,"//input[@name='login']"
     user_password = By.XPATH,"//input[@name='pass']"
-    enter = By.XPATH,"//input[@tabindex='5']"
+    enter_button = By.XPATH,"//input[@tabindex='5']"
 
 
-    def enter_user_login(self,login):
+    def enter_user_login(self,login:str):
         self.browser.find_element(*self.user_login).send_keys(login)
 
-    def enter_user_password(self,password):
+    def enter_user_password(self,password:str):
         self.browser.find_element(*self.user_password).send_keys(password)
 
+    def click_on_enter_button(self):
+        self.browser.find_element(*self.enter_button).click()
