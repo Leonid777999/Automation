1e5d0e5056454aaea2e2cd169a5f9d3afe9f5343
Leonid777999 <lskrypnikov@n-ix.com>
2023-02-28 18:47:43 +0200

    added method to PageObjectLoginPage.py imported module By, changed path to the elements user_login, User_password, enter

--- Pages/PageObjectLoginPage.py
+++ Pages/PageObjectLoginPage.py
@@ -1,16 +1,18 @@
+from selenium.webdriver.common.by import By
 from PageObjectBaseClass import BaseClass
 class LoginPage(BaseClass):
 
     def __init__(self, browser):
         super().__init__(browser)
 
-    user_login = "xpath","//input[@name='login']"
-    user_password = "xpath","//input[@name='pass']"
-    enter = "xpath","//input[@tabindex='5']"
+    user_login = By.XPATH,"//input[@name='login']"
+    user_password = By.XPATH,"//input[@name='pass']"
+    enter = By.XPATH,"//input[@tabindex='5']"
 
 
     def enter_user_login(self,login):
         self.browser.find_element(*self.user_login).send_keys(login)
 
-
+    def enter_user_password(self,password):
+        self.browser.find_element(*self.user_password).send_keys(password)
 
