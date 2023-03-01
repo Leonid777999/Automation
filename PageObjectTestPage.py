782a4f4f91afa266ff6f8fab1f89f9413fb9531b
Leonid777999 <lskrypnikov@n-ix.com>
2023-03-01 18:58:27 +0200

    Completed second task for the UI Automation with Selenium section

--- PageObjectTestPage.py
+++ PageObjectTestPage.py
@@ -1,8 +1,15 @@
 from Pages.PageObjectLoginPage import LoginPage
+from Pages.PageObjectMailBoxPage import MailBox
 
 def test_excercise(init_browser):
     site_main_page = LoginPage(init_browser)
+    mailbox_page = MailBox(init_browser)
     site_main_page.enter_user_login("automate")
     site_main_page.enter_user_password("automate123")
     site_main_page.click_on_enter_button()
+
+    last_mail = mailbox_page.get_email_title(mailbox_page.email_last)
+    before_last_mail = mailbox_page.get_email_title(mailbox_page.email_before_the_last)
+    assert "Осторожно мошенники!" in last_mail.text
+    assert "Рекомендации по безопасности Вашего аккаунта" in before_last_mail.text
 
--- Pages/PageObjectMailBoxPage.py
+++ Pages/PageObjectMailBoxPage.py
@@ -1,10 +1,16 @@
+from selenium.webdriver.common.by import By
 from Pages.PageObjectBaseClass import BaseClass
+
+
 
 class MailBox(BaseClass):
 
     def __init__(self, browser):
         super().__init__(browser)
 
-email_last = "//*[@id='mesgList']/form/div[1]/a/span[3]/span"
-email_before_the_last = "//*[@id='mesgList']/form/div[2]/a/span[3]/span"
+    email_last = By.XPATH, "//*[@id='mesgList']/form/div[1]/a/span[3]/span"
+    email_before_the_last = By.XPATH, "//*[@id='mesgList']/form/div[2]/a/span[3]/span"
+
+    def get_email_title(self,email):
+        return self.browser.find_element(*email)
 
