70a5bfc54092a3943be0cd3b26022e360e8ae73b
Leonid777999 <lskrypnikov@n-ix.com>
2023-03-03 18:10:29 +0200

    Added locators to the email details

--- PageObjectTestPage.py
+++ PageObjectTestPage.py
@@ -1,9 +1,12 @@
 from Pages.PageObjectLoginPage import LoginPage
 from Pages.PageObjectMailBoxPage import MailBox
+from Pages.PageObjectMailBoxPage import CurrentEmail
 
 def test_excercise(init_browser):
     site_main_page = LoginPage(init_browser)
     mailbox_page = MailBox(init_browser)
+    current_email = CurrentEmail(init_browser)
+
     site_main_page.enter_user_login("automate")
     site_main_page.enter_user_password("automate123")
     site_main_page.click_on_enter_button()
@@ -12,3 +15,7 @@ def test_excercise(init_browser):
     assert "Рекомендации по безопасности Вашего аккаунта" in mailbox_page.get_email_title(mailbox_page.email_before_the_last).text
 
     mailbox_page.open_email(mailbox_page.email_last)
+
+    assert "Осторожно мошенники!" in current_email.get_email_data(current_email.email_subject).text
+
+
