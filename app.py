f32116e8ad004dfb01afeccc1086b1701275aa10
Leonid777999 <lskrypnikov@n-ix.com>
2023-03-13 19:12:43 +0200

    Added app.py file and removed instances from test file to app.py file

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from selenium import webdriver
 from pages.po_login_page import LoginPage
 from pages.po_mailbox_page import MailBox, CurrentEmail
 from pages.po_new_email_page import New_Email
 from pages.po_send_status_page import Send_status
 
 class App:
-    URL = "https://i.ua"
-    def __init__(self):
+
+    def __init__(self, browser):
         # broser init
-        self.__browser = webdriver.Chrome()  # чи що ти там створюєш
+        self.__browser = browser  # чи що ти там створюєш
         # PO init
         self.login_page = LoginPage(self.__browser)
         self.mailbox_page = MailBox(self.__browser)
@@ -16,7 +15,3 @@ class App:
         self.new_email = New_Email(self.__browser)
         self.send_status = Send_status(self.__browser)
 
-    def open(self):
-        self.__browser.get(self.URL)
-    def quit(self):
-        self.__browser.quit()
---
+++ po_test_page.py
@@ -0,0 +1,32 @@
+from app import App
+
+def test_excercise(init_browser):
+
+    #create instance for test
+    app = App(init_browser)
+
+    #login to the mailbox and get titles of two letters
+    app.login_page.sign_in("automate","automate123")
+    assert "Осторожно мошенники!" in app.mailbox_page.get_title_first_mail()
+    assert "Рекомендации по безопасности Вашего аккаунта" in app.mailbox_page.get_title_second_mail()
+
+    # open specific email and check the fields "subject", "from", "to"
+    app.mailbox_page.open_email()
+    assert "Осторожно мошенники!" in app.current_email.get_data_from_subject()
+    assert "support@i.ua" in app.current_email.get_data_from_sender()
+    assert "Automate <automate@i.ua>" in app.current_email.get_data_from_receiver()
+
+    # click Create email button and switch to the creation page
+    app.current_email.create_new_mail()
+
+    # fill required fields and send email
+    app.new_email.create_new_mail("test@example.com", "check the mail", "texttexttexttextTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+
+    # open folder with the sended mails, check if the letter exists in this folder
+    app.send_status.open_sended_mails_folder()
+    assert "check the mail" in app.send_status.get_title_of_sended_mail()
+
+
+
+
+
