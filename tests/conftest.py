2568d49b27bde5b553ca844cecb51c0ec973fdbb
Leonid777999 <lskrypnikov@n-ix.com>
2023-05-22 12:21:08 +0300

    Made available to run headless browser from extra command-line argument

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from app import App
 
 @pytest.fixture(scope="session")
 def app(request):
-    app = App('chrome', request.config.getoption("--headless"))
+    app = App('firefox', request.config.getoption("--headless"))
     app.open()
     yield app
     app.close()
